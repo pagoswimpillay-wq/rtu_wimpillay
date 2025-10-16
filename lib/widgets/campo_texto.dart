ea7c0e6d575650a83a2352392417534fe9c67c6f
pagoswimpillay-wq <pagoswimpillay@gmail.com>
2025-10-16 02:11:45 -0500

    Update campo_texto.dart

--- lib/widgets/campo_texto.dart
+++ lib/widgets/campo_texto.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
 
+class CampoTexto extends StatelessWidget {
+  final TextEditingController controlador;
+  final String etiqueta;
+  final TextInputType tipo;
+
+  const CampoTexto({
+    super.key,
+    required this.controlador,
+    required this.etiqueta,
+    this.tipo = TextInputType.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controlador,
+      keyboardType: tipo,
+      decoration: InputDecoration(
+        labelText: etiqueta,
+        border: const OutlineInputBorder(),
+      ),
+    );
+  }
+}
