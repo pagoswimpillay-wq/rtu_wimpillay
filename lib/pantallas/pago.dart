ec4019684296d9667da3b052552d1a9b6b9a6ab7
pagoswimpillay-wq <pagoswimpillay@gmail.com>
2025-10-16 02:13:00 -0500

    Update pago.dart

--- lib/pantallas/pago.dart
+++ lib/pantallas/pago.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
+import 'ticket.dart';
 
+class Pago extends StatelessWidget {
+  final int cantidad;
+  const Pago({super.key, required this.cantidad});
+
+  @override
+  Widget build(BuildContext context) {
+    final monto = cantidad * 4.0;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pago')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Monto total: \$${monto.toStringAsFixed(2)}'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (_) => const Ticket()));
+              },
+              child: const Text('Generar ticket'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
