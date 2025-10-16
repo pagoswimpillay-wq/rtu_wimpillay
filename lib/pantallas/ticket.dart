184531e53a42aac2ccb301ef0ce6def80f8060ee
pagoswimpillay-wq <pagoswimpillay@gmail.com>
2025-10-16 02:12:19 -0500

    Update ticket.dart

--- lib/pantallas/ticket.dart
+++ lib/pantallas/ticket.dart
@@ -1,1 +1,40 @@
+import 'package:flutter/material.dart';
+import '../firebase_service.dart';
 
+class Ticket extends StatelessWidget {
+  const Ticket({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final servicio = ServicioFirebase();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Ticket generado')),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const Text('Nombre: José'),
+            const Text('Apellido: Wimpillay'),
+            const Text('DNI: 12345678'),
+            const SizedBox(height: 20),
+            const Text('Código QR: generado'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                servicio.crearTicket(
+                  nombre: 'José',
+                  apellido: 'Wimpillay',
+                  dni: '12345678',
+                  cantidad: 1,
+                  monto: 4.0,
+                );
+              },
+              child: const Text('Tomar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
