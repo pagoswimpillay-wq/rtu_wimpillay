c965419df9bb83a417d362550ce2af75430bdb5a
pagoswimpillay-wq <pagoswimpillay@gmail.com>
2025-10-16 02:14:18 -0500

    Update confirmacion.dart

--- lib/pantallas/confirmacion.dart
+++ lib/pantallas/confirmacion.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
 
+class Confirmacion extends StatelessWidget {
+  final bool valido;
+  const Confirmacion({super.key, required this.valido});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Confirmación')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              valido ? Icons.check_circle : Icons.cancel,
+              color: valido ? Colors.green : Colors.red,
+              size: 100,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              valido ? 'Ticket válido' : 'Ticket inválido',
+              style: const TextStyle(fontSize: 24),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
