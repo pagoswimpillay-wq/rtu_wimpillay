33ec1e8871cd08d59418c1d519d0483793ec3f88
pagoswimpillay-wq <pagoswimpillay@gmail.com>
2025-10-16 02:11:32 -0500

    Update tarjeta_ticket.dart

--- lib/widgets/tarjeta_ticket.dart
+++ lib/widgets/tarjeta_ticket.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
 
+class TarjetaTicket extends StatelessWidget {
+  final String nombre;
+  final String apellido;
+  final String dni;
+  final String codigoQR;
+
+  const TarjetaTicket({
+    super.key,
+    required this.nombre,
+    required this.apellido,
+    required this.dni,
+    required this.codigoQR,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(16),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Nombre: $nombre'),
+            Text('Apellido: $apellido'),
+            Text('DNI: $dni'),
+            Text('Código QR: $codigoQR'),
+          ],
+        ),
+      ),
+    );
+  }
+}
