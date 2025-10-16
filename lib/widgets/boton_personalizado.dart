ae4438ce02af7fbf92f77ccb3214ed05244b249d
pagoswimpillay-wq <pagoswimpillay@gmail.com>
2025-10-16 02:11:57 -0500

    Update boton_personalizado.dart

--- lib/widgets/boton_personalizado.dart
+++ lib/widgets/boton_personalizado.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
 
+class BotonPersonalizado extends StatelessWidget {
+  final String texto;
+  final VoidCallback accion;
+
+  const BotonPersonalizado({
+    super.key,
+    required this.texto,
+    required this.accion,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: accion,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        backgroundColor: Colors.green,
+      ),
+      child: Text(texto, style: const TextStyle(fontSize: 18)),
+    );
+  }
+}
