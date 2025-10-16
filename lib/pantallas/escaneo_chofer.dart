4b86a2a7e9ffeea0edb4cbb613b6e0a77c6f5419
pagoswimpillay-wq <pagoswimpillay@gmail.com>
2025-10-16 02:14:01 -0500

    Update escaneo_chofer.dart

--- lib/pantallas/escaneo_chofer.dart
+++ lib/pantallas/escaneo_chofer.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'confirmacion.dart';
+import '../firebase_service.dart';
 
+class EscaneoChofer extends StatelessWidget {
+  const EscaneoChofer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final servicio = ServicioFirebase();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Escanear QR del pasajero')),
+      body: MobileScanner(
+        onDetect: (capture) async {
+          final codigo = capture.barcodes.first.rawValue ?? '';
+          final valido = await servicio.validarQR(codigo);
+          Navigator.push(context,
+            MaterialPageRoute(builder: (_) => Confirmacion(valido: valido)));
+        },
+      ),
+    );
+  }
+}
