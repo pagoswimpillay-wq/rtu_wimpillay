19f8116580aeeeb3d50d719f3100bb596da7c09d
pagoswimpillay-wq <pagoswimpillay@gmail.com>
2025-10-16 02:13:48 -0500

    Update escaneo_pasajero.dart

--- lib/pantallas/escaneo_pasajero.dart
+++ lib/pantallas/escaneo_pasajero.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'pasajeros.dart';
 
+class EscaneoPasajero extends StatelessWidget {
+  const EscaneoPasajero({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Escanear QR del chofer')),
+      body: MobileScanner(
+        onDetect: (capture) {
+          final codigo = capture.barcodes.first.rawValue ?? '';
+          Navigator.push(context,
+            MaterialPageRoute(builder: (_) => Pasajeros(qrChofer: codigo)));
+        },
+      ),
+    );
+  }
+}
